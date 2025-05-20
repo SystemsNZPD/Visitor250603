9e4f71aaf7f673ca793ad8c4f82d10333eb18e3a
RayLee <raylee598+git@gmail.com>
2025-05-20 13:58:44 +1200

    database and app:visitor_sign_in build up

---
+++ visitor_sign_in/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+class Form(models.Model):
+    VISIT_TO_CHOICES = [
+        ('tony', 'Tony Cox-Smith'),
+        ('ann', 'Ann Mansoor'),
+        ('joanna', 'Joanna Wu'),
+        ('mele', 'Mele Tatafu'),
+        ('vicky', 'Weidi Qu'),
+        ('emily', 'Emily Donovan'),
+        ('ray', 'Ray Lee'),
+        ('ricky', 'Richard Light'),
+        ('cy', 'Chim Yun Cheng'),
+        ('julie', 'Julie Ullness'),
+    ]
+
+    visitor_name = models.CharField(max_length=80)
+    company_name = models.CharField(max_length=80)
+    phone_number = PhoneNumberField()
+    visit_to = models.CharField(
+        max_length=40,
+        choices=VISIT_TO_CHOICES,
+        help_text="Who are you visiting"
+    )
+
+    def __str__(self):
+        return f"{self.visitor_name} from {self.company_name}"
