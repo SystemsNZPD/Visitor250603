199ae048bf7ffa8de55bfc97d88cb484246d84cf
RayLee <raylee598+git@gmail.com>
2025-05-30 11:04:50 +1200

    save before apply for celery (move email sending to the background)

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,3 +128,14 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'nzexcel007@gmail.com'
 EMAIL_HOST_PASSWORD = 'fnjnvewzwbiuaguf'
+
+# app password for visitor_NZPD@outlook.com is tpfnxwgamoftctsb
+
+"""
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'nzexcel007@gmail.com'
+EMAIL_HOST_PASSWORD = 'fnjnvewzwbiuaguf'
+"""
